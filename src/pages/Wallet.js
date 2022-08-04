b38e01bc8d7fc7e207dcb7130c536fcefef810ae
HeitorFernandes <heitorsoaresfer@gmail.com>
2022-08-03 23:54:54 -0300

    Requisito 2

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,16 @@
 import React from 'react';
+// import { connect } from 'react-redux';
+import WalletForm from '../components/WalletForm';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
   render() {
-    return <div>TrybeWallet</div>;
+    return (
+      <div>
+        <Header />
+        <WalletForm />
+      </div>
+    );
   }
 }
 
